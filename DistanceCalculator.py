1aa0885c25fab6e02a93606e80ff88d58cbdb9a6
kbdelosarcos2031-cell <kbdelosarcos2031@crc.pshs.edu.ph>
2026-08-06 09:33:12 +0800

    Update DistanceCalculator.py

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -1,7 +1,10 @@
+#"import" loads a built-in library of extra math tools
 import math
 
+#Enter the first Values of
 x1=float(input("Enter value of x1: "))
 y1=float(input("Enter value of y1: "))
+
 x2=float(input("Enter value of x2: "))
 y2=float(input("Enter value of y2: "))
 
